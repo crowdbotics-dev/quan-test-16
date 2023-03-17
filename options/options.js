b6f9cc839f60784b4a08ee30477111c03020a3d3
Quan Nguyen <quan.nguyen+test@crowdbotics.com>
2023-03-17 14:45:46 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "quan-test-16",
-  url: "https://quan-test-16.botics.co",
-  api: "https://quan-test-16.botics.co/api/v1"
-}
+  "name": "quan-test-16",
+  "url": "https://quan-test-16.botics.co",
+  "api": "https://quan-test-16.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
